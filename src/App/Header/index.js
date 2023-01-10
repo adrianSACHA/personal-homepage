f2bec8c3bb8fa4cb419b786c83c875d02eca260a
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-10 14:55:44 +0100

    added component Header

---
+++ src/App/Header/index.js
@@ -0,0 +1,9 @@
+import { StyledHeader } from "./styled";
+
+const Header = ({title}) => (
+  <StyledHeader>
+    <h1>{title}</h1>
+  </StyledHeader>
+);
+
+export default Header;
---
+++ src/App/Header/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  max-width: 900px;
+  margin: 10px auto;
+`;

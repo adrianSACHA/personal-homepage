5615878881d0cdd885413b31267f2eb4dbb633dd
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-10 20:33:42 +0100

    added title to Header, deleted unwanted parts

--- src/App.js
+++ src/App.js
@@ -5,20 +5,14 @@ import Header from './App/Header';
 function App() {
   return (
     <div className="App">
-      <Header>
+      <Header title={"Adrian"} />
+    
       <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
-      </Header>
+      
     </div>
   );
 }

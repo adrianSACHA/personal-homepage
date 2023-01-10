840f62638e78da86a91843d23f4ee5ad3228bbe8
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-10 14:57:01 +0100

    added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './App/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header>
+      <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </Header>
     </div>
   );
 }

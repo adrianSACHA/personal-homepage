e4ba4f1e1d2c2edec492720a3cbf801f502efde0
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-10 20:32:43 +0100

    imported theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

d23a6aa62fb7eef17960716a00ca5c3555727681
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-11 22:33:53 +0100

    added footer component

---
+++ src/App/features/Footer/index.js
@@ -0,0 +1,14 @@
+import { Link, Memo, StyledParagraph, Wrapper } from "./styled";
+
+const Footer = () => (
+  <Wrapper>
+    <StyledParagraph>Let's talk!</StyledParagraph>
+    <Link>asachajdakiewicz@gmail.com</Link>
+    <Memo>
+      I'm always open to new projects whenever I have the time. If you have a
+      website, dashboard or mmobile app in mind and need some help to make your
+      ideas come to life, feel free to contact me.
+    </Memo>
+  </Wrapper>
+);
+export default Footer;
---
+++ src/App/features/Footer/styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 691px;
+  min-height: 282px;
+  margin: 10px;
+`;
+
+export const Memo = styled.p`
+font-weight: 400;
+font-size: 18px;
+line-height: 140%;
+letter-spacing: 0.05em;
+`;
+
+export const StyledParagraph = styled.p`
+font-weight: 700;
+font-size: 12px;
+line-height: 130%;
+/* identical to box height, or 16px */
+
+text-transform: uppercase;
+
+/* Text secondary */
+
+color: #6E7E91;
+`;
+
+export const Link = styled.a`
+font-weight: 900;
+font-size: 32px;
+line-height: 39px;
+/* identical to box height */
+
+letter-spacing: 0.05em;
+
+/* Black text */
+
+color: #252525;`;

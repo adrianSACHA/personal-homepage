71e9ca4c08d96d654bc78f3eb8c686e87741b12e
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-12 11:30:20 +0100

    formated code and added About component

--- src/App/index.js
+++ src/App/index.js
@@ -1,18 +1,16 @@
-import "./App.css";
-import { Container } from "./Common/Container/styled";
+import { Container } from "./common/Container/styled";
 import Footer from "./features/Footer";
-import Header from "./Common/Header";
-import Section from "./Common/Section";
+import Header from "./common/Header";
+import Section from "./common/Section";
+import About from "./features/About";
 
 
 function App() {
   return (
     <Container>
-      <Header title="Personal HOMEPAGE! - 🚧🚧UnderConstruction 🚧🚧" />
-      <Section title="My Skill set includes" body="React" />
-
-      <Section title="What I want to learn next" body="Typescript" />
-
+      <Header title="Personal HOMEPAGE! - 🚧🚧UnderConstruction 🚧🚧" content={<About />} />
+      <Section title="My Skill set includes 🛠️" body="React" />
+      <Section title="What I want to learn next 🚀" body="Typescript" />
       <Footer />
     </Container>
   );

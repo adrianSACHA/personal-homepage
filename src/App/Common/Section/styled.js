44b6c3f50d3be7bc153b00cdb00709dc92f2371d
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-12 12:26:51 +0100

    clear empty lines

--- src/App/Common/Section/styled.js
+++ src/App/Common/Section/styled.js
@@ -9,6 +9,7 @@ export const Sections = styled.section`
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
     0px 16px 58px rgba(9, 10, 51, 0.03);
   border-radius: 4px;
+  
 `;
 
 export const List = styled.header`

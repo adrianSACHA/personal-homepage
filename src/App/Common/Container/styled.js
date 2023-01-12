a2a005698aab3fe92ed5ad6eb8e604e84cbe02eb
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-12 08:36:34 +0100

    moved to common folder

--- src/App/Common/Container/styled.js
+++ src/App/Common/Container/styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   padding: 20px;
-  margin: 0;
+  margin: 0 auto;
   display: grid;
   gap: 72px;
-
+  justify-content: center;
 `;

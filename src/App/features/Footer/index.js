5b2f5894c82d555d280cf1433e018f421086ff43
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-13 15:24:41 +0100

    added styled Social StyledLogo and formatted code

--- src/App/features/Footer/index.js
+++ src/App/features/Footer/index.js
@@ -1,20 +1,41 @@
-import { Link, Memo, StyledParagraph, Wrapper } from "./styled";
-import {ReactComponent as GithubLogo} from './GitHub.svg';
-import {ReactComponent as LinkedInLogo} from './LinkedIn.svg';
+import {
+  GitHubLogo,
+  Link,
+  LinkedInLogo,
+  Memo,
+  Social,
+  StyledLogo,
+  StyledParagraph,
+  Wrapper,
+} from "./styled";
 
 const Footer = () => (
   <Wrapper>
     <StyledParagraph>Let's talk!</StyledParagraph>
-    <Link href="mailto:asachajdakiewicz@gmail.com">asachajdakiewicz@gmail.com</Link>
+    <Link href="mailto:asachajdakiewicz@gmail.com">
+      asachajdakiewicz@gmail.com
+    </Link>
     <Memo>
       I'm always open to new projects whenever I have the time. If you have a
       website, dashboard or mmobile app in mind and need some help to make your
       ideas come to life, feel free to contact me.
     </Memo>
-    <div>
-      <GithubLogo />
-      <LinkedInLogo />
-    </div>
+    <Social>
+      <StyledLogo
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/adrianSACHA"
+      >
+        <GitHubLogo />
+      </StyledLogo>
+      <StyledLogo
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/adrian-sachajdakiewicz/"
+      >
+        <LinkedInLogo />
+      </StyledLogo>
+    </Social>
   </Wrapper>
 );
 export default Footer;

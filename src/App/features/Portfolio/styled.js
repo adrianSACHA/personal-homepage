c8d63713e6b290b9ba814a54552c69777ab1db42
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-13 17:10:33 +0100

    added @media for mobile view

--- src/App/features/Portfolio/styled.js
+++ src/App/features/Portfolio/styled.js
@@ -5,20 +5,20 @@ export const Wrapper = styled.section`
   display: flex;
   max-width: 1216px;
   flex-direction: column;
- align-items: center;
-  
+  align-items: center;
+
   min-height: 448px;
 `;
 
 export const StyledLogo = styled.a`
-cursor: pointer;
+  cursor: pointer;
 `;
 
 export const GitHubLogo = styled(Logo)`
-  fill: #0366D6;
+  fill: #0366d6;
   width: 40px;
   height: 40px;
-  
+
   @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
     width: 32px;
     height: 32px;
@@ -32,23 +32,50 @@ export const StyledHeader = styled.h2`
   /* line-height: 36px; */
   letter-spacing: 0.05em;
   color: #252525;
-  margin: 12.98px 0  8px 0;
+  margin: 12.98px 0 8px 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-weight: 900;
+    font-size: 18px;
+    line-height: 22px;
+    /* identical to box height */
+
+    text-align: center;
+    letter-spacing: 0.05em;
+
+    /* Black text */
+
+    color: #252525;
+  }
 `;
 
 export const StyledParagraph = styled.p`
-/* height: 28px; */
+  /* height: 28px; */
 
-/* Lead paragraph */
+  /* Lead paragraph */
 
-font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 140%;
-/* identical to box height, or 28px */
-margin: 0 0 24px 0;
-letter-spacing: 0.05em;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 140%;
+  /* identical to box height, or 28px */
+  margin: 0 0 24px 0;
+  letter-spacing: 0.05em;
 
-/* Black text */
+  /* Black text */
 
-color: #252525;
+  color: #252525;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 140%;
+    /* identical to box height, or 24px */
+
+    letter-spacing: 0.05em;
+
+    /* Black text */
+
+    color: #252525;
+  }
 `;

b98785456e6e06bd6ce0ccfed82751d1dee7eee5
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-13 15:25:49 +0100

    added new styles for Social, Logo's, hovers

--- src/App/features/Footer/styled.js
+++ src/App/features/Footer/styled.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { ReactComponent as GitHub } from "./GitHub.svg";
+import { ReactComponent as LinkedIn } from "./LinkedIn.svg";
 
 export const Wrapper = styled.div`
   max-width: 691px;
@@ -7,33 +9,69 @@ export const Wrapper = styled.div`
 `;
 
 export const Memo = styled.p`
-font-weight: 400;
-font-size: 18px;
-line-height: 140%;
-letter-spacing: 0.05em;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
 `;
 
 export const StyledParagraph = styled.p`
-font-weight: 700;
-font-size: 12px;
-line-height: 130%;
-/* identical to box height, or 16px */
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 130%;
+  /* identical to box height, or 16px */
 
-text-transform: uppercase;
+  text-transform: uppercase;
 
-/* Text secondary */
+  /* Text secondary */
 
-color: #6E7E91;
+  color: #6e7e91;
 `;
 
 export const Link = styled.a`
-font-weight: 900;
-font-size: 32px;
-line-height: 39px;
-/* identical to box height */
+  font-weight: 900;
+  font-size: 32px;
+  line-height: 39px;
+  /* identical to box height */
+  cursor: pointer;
+  letter-spacing: 0.05em;
+  text-decoration: none;
+  /* Black text */
+  color: #252525;
 
-letter-spacing: 0.05em;
+  &:hover {
+    color: #0366d6;
+  }
+`;
 
-/* Black text */
+export const Social = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 24px;
 
-color: #252525;`;
+  /* position: absolute; */
+  max-width: 264px;
+  height: 48px;
+  /* left: calc(50% - 264px/2 - 476px);
+top: 2511px; */
+`;
+
+export const StyledLogo = styled.a``;
+
+export const GitHubLogo = styled(GitHub)`
+  fill: #252525;
+
+  &:hover {
+    fill: #2517eb;
+  }
+`;
+
+export const LinkedInLogo = styled(LinkedIn)`
+  fill: #252525;
+
+  &:hover {
+    fill: #2517eb;
+  }
+`;

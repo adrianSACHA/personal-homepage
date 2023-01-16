6e4bcb9bbee55c9c46ea98f4129db05a485f7319
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-16 09:54:59 +0100

    changed in Memo text

--- src/App/features/Footer/index.js
+++ src/App/features/Footer/index.js
@@ -16,7 +16,7 @@ const Footer = () => (
       asachajdakiewicz@gmail.com
     </Link>
     <Memo>
-      I'm always open to new projects whenever I have the time. If you have a
+      I'm happy to help, always seeking for solutions. Open to new projects. If you have a
       website, dashboard or mmobile app in mind and need some help to make your
       ideas come to life, feel free to contact me.
     </Memo>

53207bbb989473b815114b372c8ce67abdfd2080
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-16 14:02:01 +0100

    moved icons to new folder

---
+++ src/App/features/Footer/Social/socialData.js
@@ -0,0 +1,14 @@
+
+
+export const socialsData = [
+  {
+    name: "GitHub",
+    url: "https://github.com/",
+    Icon: styleIcon(GitHubIcon),
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/adrian-sachajdakiewicz/",
+    Icon: styleIcon(LinkedInIcon),
+  },
+];

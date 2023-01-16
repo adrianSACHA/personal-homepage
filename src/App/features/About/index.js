db7bd6e006a463f0ac8da07314b9a916a78cdd53
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-16 09:54:20 +0100

    changed in Memo text

--- src/App/features/About/index.js
+++ src/App/features/About/index.js
@@ -1,22 +1,31 @@
-import { Content, HeaderMemo, ImageOfMe, Memo, StyledButton, StyledParagraph, Wrapper } from "./styled";
+import {
+  Content,
+  HeaderMemo,
+  ImageOfMe,
+  Memo,
+  StyledButton,
+  StyledParagraph,
+  Wrapper,
+} from "./styled";
 import { ReactComponent as Message } from "./message.svg";
 
 const About = () => (
   <Wrapper>
     <ImageOfMe />
     <Content>
-    <StyledParagraph>This is</StyledParagraph>
-    <HeaderMemo>
-      Adrian Sachajdakiewicz
-    </HeaderMemo>
-    <Memo>
-      I'm always open to new projects whenever I have the time. If you have a
-      website, dashboard or mmobile app in mind and need some help to make your
-      ideas come to life, feel free to contact me.
-    </Memo>
-    <StyledButton>
-      <Message /> Hire Me
-    </StyledButton>
+      <StyledParagraph>This is</StyledParagraph>
+      <HeaderMemo>Adrian Sachajdakiewicz</HeaderMemo>
+      <Memo>
+        I am searching for a job as a junior front-end developer, where I will
+        have the opportunity to develop and gain professional experience. I am
+        an ambitious, fast-learning person who wants to constantly develop and
+        learn new technologies. In my work, I would like to use my programming
+        skills and creativity to create modern and aesthetic solutions for
+        users.
+      </Memo>
+      <StyledButton>
+        <Message /> Hire Me
+      </StyledButton>
     </Content>
   </Wrapper>
 );

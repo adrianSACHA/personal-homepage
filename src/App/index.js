a6d35456739c032d89d7e04fe595162856a198c5
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-16 11:56:10 +0100

    created FutureSkills component with styled

--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,7 @@ import Section from "./common/Section";
 import About from "./features/About";
 import Portfolio from "./features/Portfolio";
 import Skills from "./features/Skills";
+import FutureSkills from "./features/FutureSkills";
 
 
 
@@ -13,7 +14,7 @@ function App() {
     <Container>
       <Header content={<About />} />
       <Section title="My Skill set includes 🛠️" body={<Skills />} />
-      <Section title="What I want to learn next 🚀" body="Typescript" />
+      <Section title="What I want to learn next 🚀" body={<FutureSkills />} />
       <Portfolio />
       <Footer />
     </Container>

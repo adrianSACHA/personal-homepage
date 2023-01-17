86da96864d8e1568d125157f7b069257cead9e39
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-17 14:13:00 +0100

    created Tile component - not finished

---
+++ src/App/features/Portfolio/Tile/index.js
@@ -0,0 +1,11 @@
+import { Wrapper, StyledHeader } from "./styled";
+
+const Tile = () => (
+  <Wrapper>
+    <StyledHeader>Title</StyledHeader>
+    <p>Project description</p>
+    <p>Demo:</p>
+    <p>Code:</p>
+  </Wrapper>
+);
+export default Tile;
---
+++ src/App/features/Portfolio/Tile/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  color: ${({ theme }) => theme.color.white};
+  border: 6px solid rgba(209, 213, 218, 0.3);
+  min-height: 322px;
+  width: 592px;
+  border-radius: 4px;
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03);
+  text-align: center;
+`;
+export const StyledHeader = styled.h2`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29px;
+  color: ${({ theme }) => theme.color.scienceBlue};
+`;

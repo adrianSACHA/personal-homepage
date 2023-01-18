59eca31cf27c6d8fab0b4d54282164efb673bfa4
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-18 10:29:55 +0100

    💅 Wrapper on hover

--- src/App/features/Portfolio/Tile/styled.js
+++ src/App/features/Portfolio/Tile/styled.js
@@ -14,6 +14,13 @@ export const Wrapper = styled.div`
   padding: 56px;
   color: black;
 
+  &:hover {
+    border: 6px solid rgba(3, 102, 214, 0.2);
+    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+      0px 16px 58px rgba(9, 10, 51, 0.03);
+    border-radius: 4px;
+  }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
     padding: 24px;
   }
@@ -42,38 +49,37 @@ export const StyledParagraph = styled.p`
 `;
 
 export const List = styled.ul`
-list-style: none;
-font-weight: 400;
-font-size: 18px;
-line-height: 25px;
-display: flex;
-flex-direction: column;
-align-items: center;
-padding-left: 0;
+  list-style: none;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 0;
 
-color: ${({ theme }) => theme.color.slateGray};
+  color: ${({ theme }) => theme.color.slateGray};
 `;
 
 export const ListItem = styled.li`
-display: flex;
-gap: 8px;
-flex-direction: row;
-width: 100%;
-flex: 1;
-align-items: center;
+  display: flex;
+  gap: 8px;
+  flex-direction: row;
+  width: 100%;
+  flex: 1;
+  align-items: center;
 `;
 
 export const StyledLink = styled.a`
-font-style: normal;
-font-weight: 400;
-font-size: 18px;
-line-height: 25px;
-align-items: center;
-cursor: pointer;
-color: ${({ theme }) => theme.color.scienceBlue};
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+  align-items: center;
+  cursor: pointer;
+  color: ${({ theme }) => theme.color.scienceBlue};
 
-&:hover{
-  text-decoration: underline;
-}
-
+  &:hover {
+    text-decoration: underline;
+  }
 `;

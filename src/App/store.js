f8f5285d5ce3bc9861d5618bc02296c5b10d35c4
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-22 21:58:02 +0100

    🚀:Add store with Saga middleware and run rootSaga

---
+++ src/App/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSaga from "./rootSaga";
+import reposReducer from "./features/Portfolio/reposSlice";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {
+    projects: reposReducer,
+  },
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
+
+export default store;

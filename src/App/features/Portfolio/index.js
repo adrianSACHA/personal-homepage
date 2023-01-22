73b25807b516cd911a21774e1f065ed3dc701194
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-22 21:39:27 +0100

    🚀: added loading feat but not finished

--- src/App/features/Portfolio/index.js
+++ src/App/features/Portfolio/index.js
@@ -4,27 +4,43 @@ import {
   StyledLogo,
   StyledParagraph,
   Wrapper,
-  Repos
+  Repos,
 } from "./styled";
 import Tile from "./Tile";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRepos, selectRepos, selectReposStatus } from "./reposSlice";
+import { Error } from "./Info/Error";
+import { Loading } from "./Info/Loading";
 
-const Portfolio = () => (
-  <Wrapper>
-    <StyledLogo
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://github.com/adrianSACHA"
-    >
-      <GitHubLogo />
-    </StyledLogo>
-    <StyledHeader>Portfolio</StyledHeader>
-    <StyledParagraph>My recent projects</StyledParagraph>
-    <Repos>
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-    </Repos>
-  </Wrapper>
-);
+const Portfolio = () => {
+  const dispatch = useDispatch();
+  const projects = useSelector(selectRepos);
+  const status = useSelector(selectReposStatus);
+  const Status = () =>
+    ({
+      loading: <Loading />,
+      error: <Error />,
+      success: <Tile projects={projects} />,
+    }[status]);
+  useEffect(() => {
+    dispatch(fetchRepos());
+  }, [dispatch]);
+
+  return (
+    <Wrapper>
+      <StyledLogo
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/adrianSACHA"
+      >
+        <GitHubLogo />
+      </StyledLogo>
+      <StyledHeader>Portfolio</StyledHeader>
+      <StyledParagraph>My recent projects</StyledParagraph>
+      <Repos />
+      <Status />
+    </Wrapper>
+  );
+};
 export default Portfolio;

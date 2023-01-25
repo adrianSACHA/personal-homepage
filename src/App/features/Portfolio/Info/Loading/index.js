5ea38218f8577bc1d2dcf4d20045fff8a41bf751
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-25 10:07:48 +0100

    🛠️ refactor, added styled

--- src/App/features/Portfolio/Info/Loading/index.js
+++ src/App/features/Portfolio/Info/Loading/index.js
@@ -1,3 +1,11 @@
-export const Loading = () => {
-  return <div>LOADINGG......</div>;
-};
+import { Elipse, Icon, Wrapper } from "./styled";
+
+export const Loading = () => (
+  <Wrapper>
+    <Icon>
+      {" "}
+      <Elipse />
+    </Icon>
+    <p>Please wait, projects are being loaded...</p>
+  </Wrapper>
+);

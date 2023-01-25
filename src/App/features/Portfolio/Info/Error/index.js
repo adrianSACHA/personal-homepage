053e1c125967de116b161c6e6362c138c28803de
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-25 10:04:30 +0100

    🛠️: refactor error page - not finished!

--- src/App/features/Portfolio/Info/Error/index.js
+++ src/App/features/Portfolio/Info/Error/index.js
@@ -1,3 +1,11 @@
-export const Error = () => {
-  return <div>ERRRRRRRRRRRRRor......</div>;
-};
+export const Error = () => (
+  <div>
+    <p>img</p>
+    <p>Ooops! Something went wrong...</p>
+    <p>
+      Sorry, failed to load Github projects. You can check them directly on
+      Github.
+    </p>
+    <button>Go to Github</button>
+  </div>
+);

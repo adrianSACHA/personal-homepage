c293ef781f56a8fb3616f6ce67fbca99bc2fd68a
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-01-26 14:19:35 +0100

    🛠️ refactor code

--- src/App/features/Portfolio/Info/Error/index.js
+++ src/App/features/Portfolio/Info/Error/index.js
@@ -1,11 +1,16 @@
+import { StyledButton } from "../../../About/styled";
+import { ReactComponent as Danger } from "./Danger.svg";
+import { Header, Memo, Wrapper } from "./styled";
+
 export const Error = () => (
-  <div>
-    <p>img</p>
-    <p>Ooops! Something went wrong...</p>
-    <p>
-      Sorry, failed to load Github projects. You can check them directly on
-      Github.
-    </p>
-    <button>Go to Github</button>
-  </div>
+  <Wrapper>
+    <Danger />
+    <Header>Ooops! Something went wrong...</Header>
+    <Memo>
+      Sorry, failed to load Github projects.
+      <br />
+      You can check them directly on Github.
+    </Memo>
+    <StyledButton>Go{" "}to{" "}Github</StyledButton>
+  </Wrapper>
 );

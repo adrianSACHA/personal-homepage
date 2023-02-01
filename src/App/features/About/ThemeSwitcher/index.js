480e1f40460830e58f523863b01c6b5dd39dd082
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-01 09:48:13 +0100

    preparing for themeSwitch

--- src/App/features/About/ThemeSwitcher/index.js
+++ src/App/features/About/ThemeSwitcher/index.js
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Icon, Sunny, Switcher, SwitcherText, Wrapper } from "./styled";
+
+const ThemeButton = () => {
+  const dispatch = useDispatch();
+  const darkTheme = useSelector();
+};
+
+const changeTheme = () => dispatch(switchTheme());
 
 const ThemeSwitcher = () => (
   <Wrapper>
-    <SwitcherText>dark mode off</SwitcherText>
+    <SwitcherText>dark mode {!darkTheme ? "OFF" : "ON"}</SwitcherText>
     <Switcher>
       <Icon>
         <Sunny />

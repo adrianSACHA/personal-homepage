7657fc4022f67ce8f66312f48d91f844aa136450
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-07 13:37:58 +0100

    refactor whole App

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./core/App/";
 import reportWebVitals from "./reportWebVitals";
-import { GlobalStyle } from "./GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, theme } from "./theme";
 import { Provider } from "react-redux";
-import store from "./App/store";
+import store from "./core/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <GlobalStyle />
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 

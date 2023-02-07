94c6b47e974be12ccdcf60dbdd338502e63931b5
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-07 13:38:33 +0100

    correct patch

--- src/core/store.js
+++ src/core/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
-import reposReducer from "../App/features/Portfolio/reposSlice";
+import reposReducer from "../features/personalHomepage/Portfolio/reposSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 

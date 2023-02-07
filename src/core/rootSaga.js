192c0e42c72578dbe3d3182ea9decbee00dfee3d
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-07 13:38:18 +0100

    correct path

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,5 +1,5 @@
 import { all } from "@redux-saga/core/effects";
-import { reposSaga } from "../App/features/Portfolio/reposSaga";
+import { reposSaga } from "../features/personalHomepage/Portfolio/reposSaga";
 
 export default function* rootSaga() {
   yield all([reposSaga()]);

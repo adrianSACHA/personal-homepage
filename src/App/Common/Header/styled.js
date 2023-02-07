a02678ab0cf39317b983666ebd6c33910690e114
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-07 12:15:07 +0100

    deleted - moved to different dir

--- src/App/Common/Header/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const StyledHeader = styled.header`
-  max-width: 1216px;
-  margin: 0 auto;
-`;
--- src/App/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "@redux-saga/core/effects";
-import { reposSaga } from "./features/Portfolio/reposSaga";
-
-export default function* rootSaga() {
-  yield all([reposSaga()]);
-}

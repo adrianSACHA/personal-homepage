52c31d8d77da4e63aeec117511d5a5979ff7eac0
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-10 13:45:01 +0100

    🛠️ added Normalize and Homepage

--- src/core/App/index.js
+++ src/core/App/index.js
@@ -2,14 +2,18 @@ import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./theme";
 import { useSelector } from "react-redux";
-import { selectIsDarkTheme } from "../../common/themeSlice";
+import { selectIsDarkTheme } from "../../common/ThemeSwitcher/themeSlice";
+import { Normalize } from "styled-normalize";
+import { Homepage } from "../../features/personalHomepage";
 
 export const App = () => {
   const isDarkTheme = useSelector(selectIsDarkTheme);
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <Normalize />
       <GlobalStyle />
+      <Homepage />
     </ThemeProvider>
   );
 };

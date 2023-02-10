b6b04dda611ae336d2c10e058911daf5b0a041f7
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-10 13:44:20 +0100

    🛠️ added themeReducer to store

--- src/core/store.js
+++ src/core/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "./rootSaga";
 import reposReducer from "../features/personalHomepage/Portfolio/reposSlice";
+import themeReducer from "../common/ThemeSwitcher/themeSlice"
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     projects: reposReducer,
+    theme: themeReducer,
   },
   middleware: [sagaMiddleware],
 });

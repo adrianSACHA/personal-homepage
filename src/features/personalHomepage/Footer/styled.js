a4dff92fb7fe79658386c64e7ee70d07ceac602f
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-10 15:56:33 +0100

    💅 styling changes

--- src/features/personalHomepage/Footer/styled.js
+++ src/features/personalHomepage/Footer/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.footer`
   max-width: 691px;
   min-height: 282px;
   margin: 10px;
@@ -9,7 +9,7 @@ export const Wrapper = styled.div`
 export const Memo = styled.p`
   font-weight: 400;
   font-size: 18px;
-  line-height: 140%;
+  line-height: 1.4;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
     font-size: 14px;
@@ -20,7 +20,7 @@ export const Memo = styled.p`
 export const StyledParagraph = styled.p`
   font-weight: 700;
   font-size: 12px;
-  line-height: 130%;
+  line-height: 1.3;
   text-transform: uppercase;
   color: ${({ theme }) => theme.color.secondText};
 `;
@@ -31,14 +31,21 @@ export const Link = styled.a`
   line-height: 39px;
   cursor: pointer;
   text-decoration: none;
+  transition: color 0.3s;
   color: ${({ theme }) => theme.color.mainText};
 
   &:hover {
     color: ${({ theme }) => theme.color.buttonBackground};
   }
+
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
     font-size: 18px;
-    line-height: 22px;
+    line-height: 1.4;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMin}px) {
+    font-size: 14px;
+    line-height: 1.3;
   }
 `;
 
@@ -55,15 +62,16 @@ export const Social = styled.ul`
 
 export const Item = styled.li`
   gap: 24px;
-
+  list-style: none;
 `;
 
 export const StyledLogo = styled.a``;
 
-export const styleIcon = Icon => styled(Icon)`
+export const styleIcon = (Icon) => styled(Icon)`
   fill: ${({ theme }) => theme.color.mainText};
   height: 48px;
   width: 48px;
+  transition: fill 0.3s;
 
   &:hover {
     fill: ${({ theme }) => theme.color.buttonBackground};
@@ -74,18 +82,3 @@ export const styleIcon = Icon => styled(Icon)`
     height: 32px;
   }
 `;
-
-// export const LinkedInLogo = styled(LinkedIn)`
-//   fill: ${({ theme }) => theme.color.mainText};
-//   height: 48px;
-//   width: 48px;
-
-//   &:hover {
-//     fill: ${({ theme }) => theme.color.buttonBackground};
-//   }
-
-//   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
-//     width: 32px;
-//     height: 32px;
-//   }
-// `;

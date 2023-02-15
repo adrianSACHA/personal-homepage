7c311a39c87e0746864b646ac495fca668cbb023
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-15 15:32:24 +0100

    🛠️🐛 fixed target, changed path

---
+++ src/features/personalHomepage/ButtonLink/index.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 16px;
+  gap: 16px;
+  width: fit-content;
+  background: ${({ theme }) => theme.color.buttonBackground};
+  border: 1px solid ${({ theme }) => theme.color.dividerBackground};
+  border-radius: 4px;
+  color: ${({ theme }) => theme.color.buttonText};
+  font-weight: 600;
+  font-size: 20px;
+  text-decoration: none;
+  transition: box-shadow 0.3s;
+
+  &:hover {
+    box-shadow: 2px -2px 0px #8cc2ff, -2px 2px 0px #8cc2ff, 2px 2px 0px #8cc2ff,
+      -2px -2px 0px #8cc2ff;
+  }
+  &:active {
+    box-shadow: inset 0px 2px 0px #14462033;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    background: ${({ theme }) => theme.color.buttonBackground};
+    font-size: 18px;
+    line-height: 22px;
+  }
+`;

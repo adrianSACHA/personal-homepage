2cc129bd8be1dba1860174f614fe248e94fd09c4
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-17 16:40:24 +0100

    💅 grid-gap added

--- src/features/personalHomepage/Container/styled.js
+++ src/features/personalHomepage/Container/styled.js
@@ -4,10 +4,10 @@ export const Container = styled.main`
   padding: 20px;
   margin: 0 auto;
   display: grid;
-  gap: 72px;
+  grid-gap: 72px;
   justify-content: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
-    gap: 48px;
+    grid-gap: 48px;
   }
 `;

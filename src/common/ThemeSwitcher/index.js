22201c438cf0570a9a158841783b420138952a84
Adrian Sachajdakiewicz <asachajdakiewicz@gmail.com>
2023-02-20 21:29:44 +0100

    💅 format

--- src/common/ThemeSwitcher/index.js
+++ src/common/ThemeSwitcher/index.js
@@ -3,15 +3,12 @@ import { Icon, Sunny, Switcher, SwitcherText, Wrapper } from "./styled";
 import { selectIsDarkTheme, toggleTheme } from "../themeSlice";
 
 export const ThemeSwitcher = () => {
-   const isDarkTheme = useSelector(selectIsDarkTheme);
+  const isDarkTheme = useSelector(selectIsDarkTheme);
   const dispatch = useDispatch();
 
   return (
     <Wrapper>
-      <SwitcherText>
-        Dark mode{" "}
-        {isDarkTheme ? "On" : "Off"}
-      </SwitcherText>
+      <SwitcherText>Dark mode {isDarkTheme ? "On" : "Off"}</SwitcherText>
       <Switcher onClick={() => dispatch(toggleTheme())}>
         <Icon moveToRight={isDarkTheme}>
           <Sunny />
